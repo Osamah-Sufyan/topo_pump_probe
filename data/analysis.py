e7efb4101c1da08bcbdedd89a4e4be59085f8872
Osamah Sufyan <osa9m5av@yahoo.com>
2025-09-01 23:42:08 +0200

    new files added

--- data/analysis.py
+++ data/analysis.py
@@ -2,25 +2,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Import the data
-file_path = 'pump_probe_cd_t2.csv'
-data = pd.read_csv(file_path)
-data = data.sort_values(by='t2')
+file_1 = 'current_A0_left_t2_0.csv'
+file_2 = 'current_A0_left_t2_neg015.csv'
+file_3 = 'current_A0_left_t2_01.csv'
 
 
-# Plot the data
+data1 = pd.read_csv(file_1)
+data1 = data1.sort_values(by='A0')
+data2 = pd.read_csv(file_2)
+data2 = data2.sort_values(by='A0')
+data3 = pd.read_csv(file_3)
+data3 = data3.sort_values(by='A0')
+
+# delete duplicate A0 values
+# now plot the data vs A0 in  a single plot
 plt.figure(figsize=(10, 6))
-plt.plot(data['t2'], data['int1'], label='HO 1', marker='o')
-plt.plot(data['t2'], data['int5'], label='HO 5', marker='o')
-plt.plot(data['t2'], data['int7'], label='HO 7', marker='o')
-
-# Add labels, title, and legend
-plt.xlabel(r'$t_2$', fontsize=14)
-plt.ylabel('Dichroism', fontsize=14)
-plt.title('Intensity vs t2', fontsize=16)
-plt.legend(fontsize=12)
-
-plt.grid(True)
+plt.plot(data1['A0'], data1['current_y'], label='t2=0', color='blue')
+plt.plot(data2['A0'], data2['current_y'], label='t2=0.1', color='orange')
+plt.plot(data3['A0'], data3['current_y'], label='t2=-0.1', color='green')
+plt.xlabel('A0 (a.u.)')
+plt.ylabel('edge current (a.u.)')
+plt.title('Current vs A0 for different t2 values (Left Polarization)')
+plt.legend()
+plt.grid()
 
 # Show the plot
-plt.savefig('../plots/pump_probe_cd_t2.png', dpi=300)
+plt.savefig('../plots/current_A0_left.png', dpi=300)
 plt.show()
---
+++ neu_amplitudes/run_parallel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+LOGFILE="all_output.log"
+: > "$LOGFILE"
+
+max_jobs=10
+count=0
+
+for param in $(seq 0 0.05 0.4); do
+    echo "Running with param=$param" | tee -a "$LOGFILE"
+    python "current_A0.py" "$param" >> "$LOGFILE" 2>&1 &
+    ((count++))
+
+    # If we've launched max_jobs, wait for them to finish
+    if (( count % max_jobs == 0 )); then
+        wait
+    fi
+done
+
+wait  # wait for remaining jobs
